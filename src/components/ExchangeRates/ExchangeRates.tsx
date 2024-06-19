40e8c698aa7960c4f8909a9fd33cb2bb40a135f3
ManuelNovela <manuelnovela48@gmail.com>
2024-06-19 20:32:00 +0200

    Update ExchangeRates.tsx

--- src/components/ExchangeRates/ExchangeRates.tsx
+++ src/components/ExchangeRates/ExchangeRates.tsx
@@ -1,54 +1,90 @@
-import { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ExchangeImage from '../../assets/images/exchange.png';
 import DataContext from '../../services/context/DataContext';
 
+export function ExchangeRates() {
+    const { contextExchangeDate } = useContext(DataContext);
 
-export function ExchangeRates(){
+    const [amountBuy, setAmountBuy] = useState(0);
+    const [amountBuyResult, setAmountBuyResult] = useState(0);
 
-    const {contextExchangeDate, baseCurrency, countryCurrency} = useContext(DataContext);
-    
-    return (    
-      <div className="card text-body" style={{ borderRadius: '35px' }}>
-      <div className="card-body p-4">
-          <div className="d-flex">
-              <h6 className="flex-grow-1">Cambio</h6>
-              <h6>{contextExchangeDate?.date}</h6>
-          </div> 
+    const [amountSell, setAmountSell] = useState(0);
+    const [amountSellResult, setAmountSellResult] = useState(0);
 
-            <div className="row d-flex text-center justify-content-center mb-4">
-                    
-                <div className="col-md-8 col-lg-6 col-xl-5 justify-content-center mt-3 mb-2">
-                    <h6 className="display-6 mb-0 font-weight-bold"> {contextExchangeDate?.rateAmountBuy.toFixed(2) ?? "-"} </h6>
-                    <span className="small" style={{ color: '#868B94' }}>de {contextExchangeDate?.baseCurrency ?? "-"} para {contextExchangeDate?.targetCurrency ?? "-"}</span>
+    const handleBuyChange = (e) => {
+        const value = parseFloat(e.target.value);
+        if (!isNaN(value)) {
+            setAmountBuy(value);
+            setAmountBuyResult((value * contextExchangeDate.rateAmountBuy).toFixed(2));
+        }
+    };
+
+    const handleSellChange = (e) => {
+        const value = parseFloat(e.target.value);
+        if (!isNaN(value)) {
+            setAmountSell(value);
+            setAmountSellResult((value * contextExchangeDate.rateAmountSell).toFixed(2));
+        }
+    };
+
+    return (
+        <div className="card text-body" style={{ borderRadius: '35px' }}>
+            <div className="card-body p-4">
+                <div className="d-flex">
+                    <h6 className="flex-grow-1">Câmbio</h6>
+                    <h6>{contextExchangeDate?.date}</h6>
                 </div>
 
-                <div className="col-md-8 col-lg-6 col-xl-5 justify-content-center mt-3 mb-2">
-                    <h6 className="display-6 mb-0 font-weight-bold"> {contextExchangeDate?.rateAmountSell.toFixed(2) ?? "-"} </h6>
-                    <span className="small" style={{ color: '#868B94' }}>de {contextExchangeDate?.targetCurrency ?? "-"} para {contextExchangeDate?.baseCurrency ?? "-"}</span>
+                <div className="row d-flex text-center justify-content-center mb-3 mt-3">
+                    <div className="col-md-8 col-lg-6 col-xl-5 justify-content-center mt-3 mb-2">
+                        <h6 className="display-4 mb-0 font-weight-bold"> {contextExchangeDate?.rateAmountBuy.toFixed(2) ?? "-"} </h6>
+                        <span className="small" style={{ color: '#868B94' }}>de {contextExchangeDate?.baseCurrency ?? "-"} para {contextExchangeDate?.targetCurrency ?? "-"}</span>
+                    </div>
+
+                    <div className="col-md-8 col-lg-6 col-xl-5 justify-content-center mt-3 mb-2">
+                        <h6 className="display-4 mb-0 font-weight-bold"> {contextExchangeDate?.rateAmountSell.toFixed(2) ?? "-"} </h6>
+                        <span className="small" style={{ color: '#868B94' }}>de {contextExchangeDate?.targetCurrency ?? "-"} para {contextExchangeDate?.baseCurrency ?? "-"}</span>
+                    </div>
+                </div>
+
+                <div className="d-flex align-items-center">
+                    <div className="flex-grow-1" style={{ fontSize: '1rem' }}>
+                        <h6 className="flex-grow-1">Simulador</h6>
+                        <div className='mt-2'>
+                            <div> 
+                                {contextExchangeDate?.baseCurrency ?? "-"} 
+                                <input 
+                                    style={{marginRight: '10px', marginLeft: '10px', width: '60px' }}
+                                    type='number' 
+                                    min={0} 
+                                    value={amountBuy} 
+                                    onChange={handleBuyChange} 
+                                /> 
+                                = {amountBuyResult} {contextExchangeDate?.targetCurrency ?? "-"}
+                            </div>
+                        </div>
+                        <div className='mt-2'>
+                            <div> 
+                                {contextExchangeDate?.targetCurrency ?? "-"} 
+                                <input 
+                                    style={{marginRight: '10px', marginLeft: '10px', width: '60px' }}
+                                    type='number' 
+                                    min={0} 
+                                    value={amountSell} 
+                                    onChange={handleSellChange} 
+                                /> 
+                                = {amountSellResult} {contextExchangeDate?.baseCurrency ?? "-"}
+                            </div>
+                        </div>
+                        <div className='mt-2'>
+                            <span>{contextExchangeDate?.targetCurrency} X * {contextExchangeDate?.rateAmountSell.toFixed(2) ?? "rate"}  = Y {contextExchangeDate?.baseCurrency ?? "-"} </span>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={ExchangeImage} width="90px" alt="Exchange illustration" />
+                    </div>
                 </div>
             </div>
-
-
-          <div className="d-flex align-items-center">
-          <div className="flex-grow-1" style={{ fontSize: '1rem' }}>
-              <div>
-                  <i className="fas fa-wind fa-fw" style={{ color: '#868B94' }}>
-                  </i> <span className="ms-1"> Metical</span></div>
-              <div>
-                  <i className="fas fa-tint fa-fw" style={{ color: '#868B94' }}></i> 
-                  <span className="ms-1"> 1</span>
-              </div>
-              <div>
-                  <i className="fas fa-sun fa-fw" style={{ color: '#868B94' }}></i> 
-                  <span className="ms-1"> 1</span>
-              </div>
-          </div>
-          <div>
-              <img src={ExchangeImage} width="100px" alt="Exchange illustration" />
-          </div>
-          </div>
-      </div>
-  </div>
+        </div>
     );
 }
-
